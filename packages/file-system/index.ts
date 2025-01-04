747b81209ddff23691c5015d6f0ef38ce6852790
Krzysztof Pająk <chris.araneo@gmail.com>
2025-01-04 23:27:28 +0100

    feat(file-system): export additional classes, bump version

--- packages/file-system/index.ts
+++ packages/file-system/index.ts
@@ -1,10 +1,16 @@
 export * from './src/current-directory/current-directory.class';
+export * from './src/directory-creator/directory-creator.class';
+export * from './src/directory-info/directory-info.class';
 export * from './src/file/base64-file.class';
+export * from './src/file/file.class';
+export * from './src/file/json-file.class';
 export * from './src/file/text-file.class';
+export * from './src/file-finder/file-finder.class';
 export * from './src/file-reader/base64-file-reader.class';
+export * from './src/file-reader/file-reader.class';
+export * from './src/file-reader/json-file-reader.class';
 export * from './src/file-reader/read-file-result-status.enum';
 export * from './src/file-reader/text-file-reader.class';
 export * from './src/file-system/file-system.class';
-export * from './src/file-system/file-system.mock.class'; // TODO Remove
 export * from './src/file-writer/base64-file-writer.class';
 export * from './src/file-writer/text-file-writer.class';
