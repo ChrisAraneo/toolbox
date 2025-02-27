68a82a0727587f263e4030b2ed0622fdb23d60ee
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-27 23:38:55 +0100

    feat(ng-perfectionism): example use in main index file

--- packages/ng-perfectionism/index.ts
+++ packages/ng-perfectionism/index.ts
@@ -1,1 +1,25 @@
-console.log('Hello from index');
+import { CurrentDirectory, FileFinder } from '@chris.araneo/file-system';
+import { Project } from 'ts-morph';
+
+import { NgPerfectionism } from './src/ng-perfectionism';
+
+const ngPerfectionism = new NgPerfectionism();
+
+const fileFinder = new FileFinder();
+const currentDirectory = new CurrentDirectory().getCurrentDirectory();
+
+fileFinder
+  .findFile(/\.component\.ts$/, currentDirectory)
+  .subscribe(({ result }) => {
+    console.log(result);
+
+    const project = new Project();
+
+    result.forEach((path) => {
+      const sourceFile = project.addSourceFileAtPath(path);
+
+      ngPerfectionism.organizeComponentMetadataObject(sourceFile);
+
+      sourceFile.save();
+    });
+  });
