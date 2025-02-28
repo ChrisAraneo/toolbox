9b2f5c193790fb2c5b71e7964b101321f79b0384
Krzysztof Pająk <chris.araneo@gmail.com>
2025-02-28 21:15:08 +0100

    feat(ng-perfectionism): add shebang, add console logs, throw error when no file found

--- packages/ng-perfectionism/index.ts
+++ packages/ng-perfectionism/index.ts
@@ -1,22 +1,36 @@
+#!/usr/bin/env node
+
 import { CurrentDirectory, FileFinder } from '@chris.araneo/file-system';
 import { Project } from 'ts-morph';
 
 import { NgPerfectionism } from './src/ng-perfectionism';
+
+console.log('NgPerfectionism');
 
 const ngPerfectionism = new NgPerfectionism();
 
 const fileFinder = new FileFinder();
 const currentDirectory = new CurrentDirectory().getCurrentDirectory();
 
+console.log('Current working directory:', currentDirectory);
+console.log('Searching component files...');
+
 fileFinder
   .findFile(/\.component\.ts$/, currentDirectory)
   .subscribe(({ result }) => {
-    console.log(result);
-
     const project = new Project();
 
     result.forEach((path) => {
-      const sourceFile = project.addSourceFileAtPath(path);
+      let sourceFile = project.addSourceFileAtPathIfExists(path);
+      const relativePath = path.replace(currentDirectory, '');
+
+      if (!sourceFile) {
+        sourceFile = project.addSourceFileAtPathIfExists(relativePath);
+      }
+
+      if (!sourceFile) {
+        throw new Error(`Can't read source file: ${path} (${relativePath})`);
+      }
 
       ngPerfectionism.organizeComponentMetadataObject(sourceFile);
 
