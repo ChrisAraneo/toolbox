b4d2df34cb97a46133b139f1fa574e490d4d20c6
Krzysztof Pająk <chris.araneo@gmail.com>
2025-03-01 17:00:11 +0100

    feat(ng-perfectionism): use #findFiles

--- packages/ng-perfectionism/index.ts
+++ packages/ng-perfectionism/index.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { CurrentDirectory, FileFinder } from '@chris.araneo/file-system';
+import { CurrentDirectory } from '@chris.araneo/file-system';
 import { Project } from 'ts-morph';
 
 import { NgPerfectionism } from './src/ng-perfectionism';
@@ -9,31 +9,28 @@ console.log('NgPerfectionism');
 
 const ngPerfectionism = new NgPerfectionism();
 
-const fileFinder = new FileFinder();
-const currentDirectory = new CurrentDirectory().getCurrentDirectory();
+const directory = new CurrentDirectory().getCurrentDirectory();
 
-console.log('Current working directory:', currentDirectory);
+console.log('Current working directory:', directory);
 console.log('Searching component files...');
 
-fileFinder
-  .findFile(/\.component\.ts$/, currentDirectory)
-  .subscribe(({ result }) => {
-    const project = new Project();
+ngPerfectionism.findFiles(directory).then(({ result }) => {
+  const project = new Project();
 
-    result.forEach((path) => {
-      let sourceFile = project.addSourceFileAtPathIfExists(path);
-      const relativePath = path.replace(currentDirectory, '');
+  result.forEach((path) => {
+    let sourceFile = project.addSourceFileAtPathIfExists(path);
+    const relativePath = path.replace(directory, '');
 
-      if (!sourceFile) {
-        sourceFile = project.addSourceFileAtPathIfExists(relativePath);
-      }
+    if (!sourceFile) {
+      sourceFile = project.addSourceFileAtPathIfExists(relativePath);
+    }
 
-      if (!sourceFile) {
-        throw new Error(`Can't read source file: ${path} (${relativePath})`);
-      }
+    if (!sourceFile) {
+      throw new Error(`Can't read source file: ${path} (${relativePath})`);
+    }
 
-      ngPerfectionism.organizeComponentMetadataObject(sourceFile);
+    ngPerfectionism.organizeComponentMetadataObject(sourceFile);
 
-      sourceFile.save();
-    });
+    sourceFile.save();
   });
+});
