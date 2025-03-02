f1cc78fda0c531b2f27e2481a2950dede23900bd
Krzysztof Pająk <chris.araneo@gmail.com>
2025-03-02 18:30:38 +0100

    feat(email-service): init package, create class and add base service logic

---
+++ packages/email-service/src/email-service.ts
@@ -0,0 +1,100 @@
+import { Logger } from '@chris.araneo/logger';
+import express from 'express';
+import Mustache from 'mustache';
+import Mailjet, { Client } from 'node-mailjet';
+
+export class EmailService {
+  private env: Record<string, string | undefined>;
+
+  constructor(private readonly logger: Logger) {
+    this.env = { ...process.env };
+
+    this.logger.info('Email Service v0.0.1');
+
+    this.logger.debug(
+      `Environmental variables: ${JSON.stringify({ ...this.env, ['MJ_APIKEY_PRIVATE']: undefined })}`,
+    );
+  }
+
+  listen(port: number): void {
+    const {
+      MJ_APIKEY_PUBLIC,
+      MJ_APIKEY_PRIVATE,
+      SENDER,
+      NAME,
+      SUBJECT,
+      RECEIVER,
+      TEXT_TEMPLATE,
+      HTML_TEMPLATE,
+    } = this.env;
+
+    const app = express();
+
+    let mailjet: Client;
+
+    if (!MJ_APIKEY_PUBLIC) {
+      return this.logger.error(`MJ_APIKEY_PUBLIC is undefined`);
+    }
+
+    if (!MJ_APIKEY_PRIVATE) {
+      return this.logger.error(`MJ_APIKEY_PRIVATE is undefined`);
+    }
+
+    try {
+      mailjet = Mailjet.apiConnect(MJ_APIKEY_PUBLIC, MJ_APIKEY_PRIVATE);
+    } catch (error: unknown) {
+      this.logger.error('Mailjet API connect error: ' + JSON.stringify(error));
+    }
+
+    app.get('/', (request, respone) => {
+      const body = request.body;
+
+      if (!TEXT_TEMPLATE) {
+        return this.logger.error(`TEXT_TEMPLATE is undefined`);
+      }
+
+      const text = Mustache.render(TEXT_TEMPLATE, body);
+
+      if (!HTML_TEMPLATE) {
+        return this.logger.error(`HTML_TEMPLATE is undefined`);
+      }
+
+      const html = Mustache.render(HTML_TEMPLATE, body);
+
+      mailjet
+        .post('send', { version: 'v3.1' })
+        .request({
+          Messages: [
+            {
+              From: {
+                Email: SENDER,
+                Name: NAME,
+              },
+              To: [
+                {
+                  Email: RECEIVER,
+                },
+              ],
+              Subject: SUBJECT,
+              TextPart: text,
+              HTMLPart: html,
+            },
+          ],
+        })
+        .then((result) => {
+          this.logger.info(`Result: ${result.body}`);
+
+          respone.send({ status: 'success' });
+        })
+        .catch((error) => {
+          this.logger.error(`Result: ${JSON.stringify(error)}`);
+
+          respone.send({ status: 'error', message: error });
+        });
+    });
+
+    app.listen(port);
+
+    this.logger.info(`Server running at port ${port}`);
+  }
+}
