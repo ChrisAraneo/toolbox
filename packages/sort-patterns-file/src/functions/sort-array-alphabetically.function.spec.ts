7b251ec17e0531bda7e8e99045d04859efdb15a1
Krzysztof Pająk <chris.araneo@gmail.com>
2025-03-25 23:13:37 +0100

    test(sort-patterns-file): update sortArrayAlphabetically tests

--- packages/sort-patterns-file/src/functions/sort-array-alphabetically.function.spec.ts
+++ packages/sort-patterns-file/src/functions/sort-array-alphabetically.function.spec.ts
@@ -1,33 +1,33 @@
 import { sortArrayAlphabetically } from './sort-array-alphabetically.function';
 
 describe('sortArrayAlphabetically', () => {
-  it('should return empty array when given empty array', () => {
-    expect(sortArrayAlphabetically([])).toEqual([]);
+  it('should return empty array when given an empty array', () => {
+    const input: string[] = [];
+    sortArrayAlphabetically(input);
+    expect(input).toEqual([]);
   });
 
   it('should return sorted array of strings', () => {
-    expect(sortArrayAlphabetically(['banana', 'apple', 'cherry'])).toEqual([
-      'apple',
-      'banana',
-      'cherry',
-    ]);
-    expect(sortArrayAlphabetically(['banana', 'Apple', 'cherry'])).toEqual([
-      'Apple',
-      'banana',
-      'cherry',
-    ]);
-    expect(sortArrayAlphabetically(['banana', 'apple', 'banana'])).toEqual([
-      'apple',
-      'banana',
-      'banana',
-    ]);
+    const input1: string[] = ['banana', 'apple', 'cherry'];
+    sortArrayAlphabetically(input1);
+    expect(input1).toEqual(['apple', 'banana', 'cherry']);
+
+    const input2: string[] = ['banana', 'Apple', 'cherry'];
+    sortArrayAlphabetically(input2);
+    expect(input2).toEqual(['Apple', 'banana', 'cherry']);
+
+    const input3: string[] = ['banana', 'apple', 'banana'];
+    sortArrayAlphabetically(input3);
+    expect(input3).toEqual(['apple', 'banana', 'banana']);
   });
 
   it('should mutate the input array', () => {
-    const input = ['banana', 'apple', 'cherry'];
+    const input: string[] = ['banana', 'apple', 'cherry'];
+    const originalInput: string[] = [...input];
 
     sortArrayAlphabetically(input);
 
     expect(input).toEqual(['apple', 'banana', 'cherry']);
+    expect(input).not.toEqual(originalInput);
   });
 });
