05b9c7b582945a9e6e800dd77547ce9fede20b1b
Krzysztof Pająk <chris.araneo@gmail.com>
2025-04-17 22:04:03 +0200

    feat(email-service): use Mailersend API, update env var keys

--- packages/email-service/src/email-service.class.ts
+++ packages/email-service/src/email-service.class.ts
@@ -2,16 +2,18 @@ import { Logger } from '@chris.araneo/logger';
 import Express from 'express';
 import { ParamsDictionary, Request, Response } from 'express-serve-static-core';
 import { Server } from 'http';
+import Mustache from 'mustache';
+import fetch from 'node-fetch';
 import { ParsedQs } from 'qs';
 
-// import { EnvVarKey } from './env-var-key.type';
+import { EnvVarKey } from './env-var-key.type';
 
 export class EmailService {
-  // private env?: Record<string, string | undefined>;
+  private env?: Record<string, string | undefined>;
   private server?: Server;
 
   constructor(private readonly logger: Logger) {
-    this.logger.info('Email Service v0.0.11');
+    this.logger.info('Email Service v0.0.12');
 
     this.logger.debug(
       `Environmental variables: ${JSON.stringify({ ...process.env, ['MJ_APIKEY_PRIVATE']: undefined })}`,
@@ -45,48 +47,113 @@ export class EmailService {
       ParsedQs,
       Record<string, unknown>
     >,
-    respone: Response<unknown, Record<string, unknown>, number>,
+    response: Response<unknown, Record<string, unknown>, number>,
   ): void {
-    // const { TEXT_TEMPLATE, HTML_TEMPLATE, SENDER, NAME, RECEIVER, SUBJECT } =
-    //   this.getEnvironmentVariablesOrThrow();
+    const {
+      TEXT_TEMPLATE,
+      HTML_TEMPLATE,
+      FROM_EMAIL,
+      FROM_NAME,
+      TO_EMAIL,
+      TO_NAME,
+      SUBJECT,
+      MAILERSEND_TOKEN,
+    } = this.getEnvironmentVariablesOrThrow();
 
-    // const body = request.body;
+    const requestBody = request.body;
+    let text = '';
+    let html = '';
+    let error: unknown = null;
 
-    // const text = Mustache.render(TEXT_TEMPLATE, body);
-    // const html = Mustache.render(HTML_TEMPLATE, body);
+    try {
+      text = Mustache.render(TEXT_TEMPLATE, requestBody);
+    } catch (e: unknown) {
+      error = e;
+    }
 
-    respone.send({
-      status: 'error',
-      message: 'Email sending service client not implemented yet',
-    });
+    try {
+      html = Mustache.render(HTML_TEMPLATE, requestBody);
+    } catch (e: unknown) {
+      error = e;
+    }
+
+    if (error) {
+      console.error(error);
+      this.logger.error(JSON.stringify(error));
+
+      return;
+    }
+
+    const body = {
+      from: {
+        email: FROM_EMAIL,
+        name: FROM_NAME,
+      },
+      to: [
+        {
+          email: TO_EMAIL,
+          name: TO_NAME,
+        },
+      ],
+      subject: SUBJECT,
+      text: text,
+      html: html,
+      personalization: [],
+    };
+
+    fetch('https://api.mailersend.com/v1/email', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${MAILERSEND_TOKEN}`,
+      },
+    })
+      .then((response) => response.json())
+      .then(() => {
+        response.send({ status: 'success', message: 'E-mail sent' });
+      })
+      .catch((error: unknown) => {
+        console.error(error);
+        this.logger.error(JSON.stringify(error));
+
+        response.send({
+          status: 'error',
+          message: 'Error while sending email',
+        });
+      });
   }
 
-  // private getEnvironmentVariablesOrThrow(): Record<EnvVarKey, string> {
-  //   if (!this.env) {
-  //     this.env = { ...process.env };
-  //   }
+  private getEnvironmentVariablesOrThrow(): Record<EnvVarKey, string> {
+    if (!this.env) {
+      this.env = { ...process.env };
+    }
 
-  //   const result: Record<EnvVarKey, string> = {
-  //     SENDER: '',
-  //     NAME: '',
-  //     SUBJECT: '',
-  //     RECEIVER: '',
-  //     TEXT_TEMPLATE: '',
-  //     HTML_TEMPLATE: '',
-  //   };
+    const result: Record<EnvVarKey, string> = {
+      FROM_EMAIL: '',
+      FROM_NAME: '',
+      TO_EMAIL: '',
+      TO_NAME: '',
+      NAME: '',
+      SUBJECT: '',
+      RECEIVER: '',
+      TEXT_TEMPLATE: '',
+      HTML_TEMPLATE: '',
+      MAILERSEND_TOKEN: '',
+    };
 
-  //   (Object.keys(result) as EnvVarKey[]).forEach((key: EnvVarKey) => {
-  //     const value = this.env?.[key];
+    (Object.keys(result) as EnvVarKey[]).forEach((key: EnvVarKey) => {
+      const value = this.env?.[key];
 
-  //     if (!value) {
-  //       throw new Error(
-  //         `${key} environment variable is undefined but it is required`,
-  //       );
-  //     }
+      if (!value) {
+        throw new Error(
+          `${key} environment variable is undefined but it is required`,
+        );
+      }
 
-  //     result[key] = value;
-  //   });
+      result[key] = value;
+    });
 
-  //   return result;
-  // }
+    return result;
+  }
 }
--- packages/email-service/src/env-var-key.type.ts
+++ packages/email-service/src/env-var-key.type.ts
@@ -1,7 +1,11 @@
 export type EnvVarKey =
-  | 'SENDER'
+  | 'FROM_EMAIL'
+  | 'FROM_NAME'
+  | 'TO_EMAIL'
+  | 'TO_NAME'
   | 'NAME'
   | 'SUBJECT'
   | 'RECEIVER'
   | 'TEXT_TEMPLATE'
-  | 'HTML_TEMPLATE';
+  | 'HTML_TEMPLATE'
+  | 'MAILERSEND_TOKEN';
