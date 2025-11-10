7aa34d4ecaa1af84d55b09ae33bbd51493f3c744
Krzysztof Pająk <chris.araneo@gmail.com>
2025-11-10 21:13:11 +0100

    refactor(file-decryptor): refactor into class, rename imports, rename param

--- packages/crypto/src/file-decryptor/file-decryptor.class.ts
+++ packages/crypto/src/file-decryptor/file-decryptor.class.ts
@@ -1,23 +1,23 @@
 import { Base64File } from '@chris.araneo/file-system';
-import CryptoJS from 'crypto-js';
-import CryptoAES from 'crypto-js/aes';
+import cryptoJs from 'crypto-js';
+import cryptoAes from 'crypto-js/aes';
 
 import { EncryptedFile } from '../encrypted-file/encrypted-file.class';
 
-export const FileDecryptor = {
+export class FileDecryptor {
   decryptBase64Files(files: EncryptedFile[], secretKey: string): Base64File[] {
-    return files.map((files: EncryptedFile) =>
-      this.decryptBase64File(files, secretKey),
+    return files.map((file: EncryptedFile) =>
+      this.decryptBase64File(file, secretKey),
     );
-  },
+  }
 
   decryptBase64File(file: EncryptedFile, secretKey: string): Base64File {
     return new Base64File(
       file.getPath(),
-      CryptoAES.decrypt(file.getContent(), secretKey).toString(
-        CryptoJS.enc.Utf8,
+      cryptoAes.decrypt(file.getContent(), secretKey).toString(
+        cryptoJs.enc.Utf8,
       ),
       file.getModifiedDate(),
     );
-  },
+  }
 };
