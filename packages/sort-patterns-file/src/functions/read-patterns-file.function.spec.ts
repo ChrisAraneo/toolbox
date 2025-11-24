8a4d0fc12b42c968c5393de209007da25c9c7965
Krzysztof Pająk <chris.araneo@gmail.com>
2025-11-24 21:29:50 +0100

    style(sort-patterns-file): change require to import

--- packages/sort-patterns-file/src/functions/read-patterns-file.function.spec.ts
+++ packages/sort-patterns-file/src/functions/read-patterns-file.function.spec.ts
@@ -1,7 +1,6 @@
-import { readPatternsFile } from './read-patterns-file.function';
+import fs from 'node:fs';
 
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const fs = require('node:fs');
+import { readPatternsFile } from './read-patterns-file.function';
 
 jest.mock('fs', () => ({
   readFile: jest.fn(),
@@ -10,7 +9,7 @@ jest.mock('fs', () => ({
 describe('readPatternsFile', () => {
   it('should resolve with the correct array of lines from the file', async () => {
     const content = 'pattern1\npattern2\npattern3\r\n';
-    (fs.readFile as jest.Mock).mockImplementation((_, __, callback) => {
+    (fs.readFile as unknown as jest.Mock).mockImplementation((_, __, callback) => {
       callback(null, content);
     });
 
@@ -21,7 +20,7 @@ describe('readPatternsFile', () => {
 
   it('should reject with an error if readFile fails', async () => {
     const error = new Error('File not found');
-    (fs.readFile as jest.Mock).mockImplementation((_, __, callback) =>
+    (fs.readFile as unknown as jest.Mock).mockImplementation((_, __, callback) =>
       callback(error, ''),
     );
 
@@ -36,7 +35,7 @@ describe('readPatternsFile', () => {
     String.raw`should ignore empty lines and lines with only \r or \n characters`,
     async () => {
       const content = '\n\npattern1\n\npattern2\n\r\n\r\n';
-      (fs.readFile as jest.Mock).mockImplementation((_, __, callback) =>
+      (fs.readFile as unknown as jest.Mock).mockImplementation((_, __, callback) =>
         callback(null, content),
       );
 
@@ -48,7 +47,7 @@ describe('readPatternsFile', () => {
 
   it(String.raw`should handle \r and \n characters correctly`, async () => {
     const content = 'pattern1\r\npattern2\npattern3\r';
-    (fs.readFile as jest.Mock).mockImplementation((_, __, callback) =>
+    (fs.readFile as unknown as jest.Mock).mockImplementation((_, __, callback) =>
       callback(null, content),
     );
 
