1401eb6f788f3ac455af442a8c1634ef3af6546d
Krzysztof Pająk <chris.araneo@gmail.com>
2025-11-30 16:57:51 +0100

    refactor(sort-patterns-file): use find instead of forEach in isArrayDiff

--- packages/sort-patterns-file/src/functions/is-array-diff.function.ts
+++ packages/sort-patterns-file/src/functions/is-array-diff.function.ts
@@ -1,15 +1,11 @@
+import { isUndefined } from 'lodash';
+
 export const isArrayDiff = (a: string[], b: string[]): boolean => {
   if (a.length !== b.length) {
     return true;
   }
 
-  let isDiff = false;
-
-  for (let i = 0; i < a.length && !isDiff; i++) {
-    if (a[i] !== b[i]) {
-      isDiff = true;
-    }
-  }
-
-  return isDiff;
+  return !isUndefined(
+    a.find((value, index) => value !== b[index]),
+  );
 };
