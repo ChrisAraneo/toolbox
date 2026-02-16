58a7b84b43ebbcb29197d1b25e31220650bbf921
Krzysztof Pająk <chris.araneo@gmail.com>
2026-02-16 23:55:07 +0100

    fix(file-system): update import

--- packages/file-system/src/file-reader/functions/read-file.function.ts
+++ packages/file-system/src/file-reader/functions/read-file.function.ts
@@ -1,5 +1,5 @@
 import { FileSystem } from 'src/file-system/file-system.class';
-import { createSuccessResult } from './create-success-read-file-result.function';
+import { createSuccessResult } from './create-success-result.function';
 import { catchError, from, map } from 'rxjs';
 import { createErrorResult } from './create-error-result.function';
 import { FILE_CONTENT_READING_ERROR_MESSAGE } from '../consts/file-reader.consts';
