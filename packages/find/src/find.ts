12a4d67e488c25a913c04ccbba06fecde7dfcf92
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-03 22:55:14 +0200

    refactor(find): use chain

--- packages/find/src/find.ts
+++ packages/find/src/find.ts
@@ -1,6 +1,7 @@
 // Stryker disable all
 
 import { glob } from 'glob';
+import { chain } from 'lodash-es';
 
 import { createGlobOptions } from './functions/create-glob-options.function';
 import { getRootDir } from './functions/get-root-dir.function';
@@ -13,7 +14,7 @@ import { FindOptions } from './interfaces/find-options.interface';
  * @param pattern - Glob pattern to match files and directories (e.g., '**\/*.ts', 'src/**', '*.json')
  * @param options - Optional configuration object
  * @param options.root - Root directory to search from (defaults to process.cwd())
- * @param options.cwd - Alias for root (for backward compatibility)
+ * @param options.cwd - Alias for root
  * @param options.ignore - Array of patterns to ignore (e.g., ['node_modules/**', 'dist/**'])
  * @param options.dot - Include dotfiles (defaults to false)
  * @returns Promise that resolves to an array of matching file and directory paths
@@ -36,10 +37,7 @@ import { FindOptions } from './interfaces/find-options.interface';
 export const find = async (
   pattern: string,
   options?: FindOptions,
-): Promise<string[]> => {
-  const rootDir = getRootDir(options);
-  const globOptions = createGlobOptions(options);
-  const normalize = normalizePaths(rootDir);
-
-  return glob(pattern, globOptions).then(normalize);
-};
+): Promise<string[]> =>
+  chain(await glob(pattern, createGlobOptions(options)))
+    .thru(normalizePaths(getRootDir(options)))
+    .value();
