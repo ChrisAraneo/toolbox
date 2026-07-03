08ff3962263ca8d1c9635b80f272ec44b6e38403
Krzysztof Pająk <chris.araneo@gmail.com>
2026-07-03 22:17:54 +0200

    style(find): format code

--- packages/find/src/functions/get-root-dir.function.ts
+++ packages/find/src/functions/get-root-dir.function.ts
@@ -3,5 +3,4 @@ import { FindOptions } from '../interfaces/find-options.interface';
 /**
  * Extracts the root directory from options
  */
-export const getRootDir = (options?: FindOptions): string | undefined =>
-  options?.root ?? options?.cwd;
+export const getRootDir = (options?: FindOptions): string | undefined => options?.root ?? options?.cwd;
--- packages/find/src/functions/normalize-paths.function.ts
+++ packages/find/src/functions/normalize-paths.function.ts
@@ -1,11 +1,9 @@
 import { normalize } from 'node:path';
 
-import { map } from 'lodash';
+import { map } from 'lodash-es';
 
 /**
  * Normalizes an array of paths
  */
 export const normalizePaths =
-  (rootDir?: string) =>
-  (paths: readonly string[]): string[] =>
-    map(paths, (path) => normalize(`${rootDir ? `${rootDir}/` : ''}${path}`));
+  (rootDir?: string) => (paths: readonly string[]): string[] => map(paths, (path) => normalize(`${rootDir ? `${rootDir}/` : ''}${path}`));
